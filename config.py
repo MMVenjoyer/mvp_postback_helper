003210783ed9cb00c511e627676f6c6e2f4d7c9b
s74rdus7 <newquest72@gmail.com>
2025-11-09 00:24:49 +0500

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ DB_CONFIG = {
 # KEITARO CONFIGURATION
 # ===========================================
 KEITARO_DOMAIN = os.getenv("KEITARO_DOMAIN", "https://test.com")
-KEITARO_API_KEY = os.getenv("KEITARO_API_KEY", "test")
+KEITARO_ADMIN_API_KEY = os.getenv("KEITARO_API_KEY", "test")
 
 # ===========================================
 # SYNC SETTINGS
